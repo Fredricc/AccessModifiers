ee9cc93252fc55eaabde097a84fa20cd1cb2e067
Fredrick Njuguna <kfred45.fk@gmail.com>
2024-01-15 03:37:41 +0300

    Add project files.

---
+++ Customer.cs
@@ -0,0 +1,21 @@
+﻿namespace AccessModifiers
+{
+    public class Customer
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public void Promote()
+        {
+            var rating = CalculateRating(excludingOrder: true);
+            if (rating == 0)
+                Console.WriteLine("Promoted to level 1");
+            else
+                Console.WriteLine("Promoted to level 2");
+        }
+
+        private int CalculateRating(bool excludingOrder)
+        {
+            return 0;
+        }
+    }
+}
